fd3d78382dfe37d63742d6213ecc338fd4211afe
Arthur Schuelter <arthur.schuelter@gmail.com>
2018-02-28 17:55:07 -0300

    February 23

--- Image-Processing/1-Introduction/xadrez.py
+++ Image-Processing/1-Introduction/xadrez.py
@@ -20,25 +20,7 @@ def GeraTabuleiro(W,H,T):
 
     return img
 
-def GeraTabuleiro2(W,H,T):
 
-    numeroQuadrados = W/T
-
-    img = np.zeros((W,H), dtype = np.uint8)
-
-    print img
-
-    for i in range(0, numeroQuadrados):
-        ni = i * T
-        for j in range(0, numeroQuadrados):
-           nj = j * T
-           for k in range(ni, ni+T):
-               for l in range(nj, nj+T):
-                    img[k][l] = ((i+j)%2) * 255
-
-    return img
-
-
-tabu = GeraTabuleiro2(256, 256, 64)
+tabu = GeraTabuleiro(256, 256, 64)
 print tabu
 im.show(tabu)
