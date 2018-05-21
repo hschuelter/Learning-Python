fbc7a0fa5ca7d3f1913b22b19b8a8718b3922e6c
hschuelter <arthur.schuelter@gmail.com>
2018-05-21 10:42:14 -0300

    Simple wordcount in python

---
+++ Basics/random-word-generator.py
@@ -0,0 +1,17 @@
+import random
+
+def main():
+	file = open("teste.txt", "w", encoding="utf-8-sig")
+
+	for i in range(0, 10000):
+		a = random.randint(0, 5)
+
+		if a == 0: file.write("Dog ")
+		if a == 1: file.write("Cat ")
+		if a == 2: file.write("Bird ")
+		if a == 3: file.write("Train ")
+		if a == 4: file.write("Bus ")
+		if a == 5: file.write("Boat ")
+
+if __name__== '__main__':
+	main()
---
+++ Basics/wordcount.py
@@ -0,0 +1,34 @@
+import argparse
+
+def rewind(file):
+	file.seek(0)
+
+def main(args):
+	file = open(args.file, 'r', encoding = 'utf-8-sig')
+	print('Your file is: >' + args.file + '<')
+	
+	rewind(file)
+	wordcount = {}
+	text = file.read().split()
+
+	for word in text:
+		if word in wordcount:
+			wordcount[word]+= 1
+		else:
+			wordcount[word] = 1
+
+	for word, num in sorted(wordcount.items()):
+		print( word + '\t' + str(num) )
+
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Wordcount")
+	parser.add_argument('--file', '-f',
+						type=str,
+						help="Select the file to be analyzed",
+						default="teste.txt")
+
+	args = parser.parse_args()
+
+	main(args)
