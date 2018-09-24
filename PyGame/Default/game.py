092fd6b54904a4f6259cc6f85b109f6dbd444fb1
hschuelter <arthur.schuelter@gmail.com>
2018-09-24 17:00:12 -0300

    FPS

--- PyGame/Default/game.py
+++ PyGame/Default/game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as pg
 import random
 ##
 from Settings import Settings
@@ -6,48 +6,62 @@ from Settings import Settings
 
 def setup():
 	# resolution = (1280, 720)
-	resolution = (720, 540)
-	settings = Settings(resolution, 60)
+	resolution = (960, 720)
 
-	pygame.display.set_caption('Joguito')
-	screen  = pygame.display.set_mode(resolution)
+	pg.init()
+
+	settings = Settings(resolution, 60)
+	pg.display.set_caption('Joguito')
+
+	screen  = pg.display.set_mode(resolution)
+
+	settings.font = pg.font.Font(None, 30)
+	settings.clock = pg.time.Clock()
 
 	return screen, settings
 
-
-def draw(screen, settings):
-	background = (0, 0, 0)
+def chess(screen, settings):
 	color = (180, 180, 255)
-	black = (10,10,10)
-	white = (230,230,230)
+	black = (30,30,30)
+	white = (220,220,220)
 
 	w = settings.resolution[0]
 	h = settings.resolution[1]
 
-	y_num = 20
-	x_num = 20
+	num = 8
 	thicc = 10
 	cont = 0
 
-	screen.fill(background)
-	for y in range(0, y_num):
-		for x in range(0, x_num):
+	for y in range(0, num):
+		for x in range(0, num):
 
 			if(cont % 2 == 0):
 				color = white
 			else:
 				color = black
-			pygame.draw.rect(screen, color,
-							( (w * x) / x_num, 	 # X Starting point
-							(h * y) / y_num,	 # Y Starting point
-							(w / x_num),		 # X size
-							(h / y_num)))		 # Y size
+
+			pg.draw.rect(screen, color,
+							( ((w-h)/2) + (h * x) / num, 	# X Starting point
+							(h * y) / num,	 				# Y Starting point
+							(h / num),		 				# X size
+							(h / num)))		 				# Y size
 			cont += 1
-	update()
+		cont +=1
+
+def draw(screen, settings):
+	background = (0, 0, 0)
+
+	screen.fill(background)
+	chess(screen, settings)
+	update(screen, settings)
 
 
-def update():
-	pygame.display.flip()
+def update(screen, settings):
+	pg.draw.rect(screen, pg.Color(0, 0, 0, 50), (50, 50, 75, 20))
+	fps = settings.font.render("FPS: " + str(int(settings.clock.get_fps())), True, pg.Color('white'))
+	screen.blit(fps, (50, 50))
+	pg.display.flip()
+	settings.clock.tick(settings.framerate)
 
 def main():
 	print("Starting...")
@@ -57,8 +71,8 @@ def main():
 	running = True
 	while(running):
 		draw(screen, settings)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
 				running = False
 
 if __name__ == "__main__": main()
